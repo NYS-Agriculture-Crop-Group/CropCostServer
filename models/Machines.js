c87f5414bb6f02b857c7d83c702fe7a4d5ebba1d
Gabriel Vande Hei <gfvandehei@gmail.com>
2020-04-19 17:14:14 -0400

    added machine api

--- models/Machines.js
+++ models/Machines.js
@@ -5,31 +5,89 @@ const jwt = require('jsonwebtoken');
 const { Schema } = mongoose;
 
 const MachineSchema = new Schema({
-    ownerid: String,
-    machineName: String,
-    efficiency: Number,
-    efficiency_unit: String,
-    haulingCapacity: Number,
-    haulingCapacityUnits: String,
-    working_width: Number,
-    operating_cost: Number,
-    operatingCostUnits: String,
-    purchaseDate: Date,
-    purchasePrice: Number,
-    salvagePrice: Number,
-    depriciation: Number,
-    paymentsPerYear: Number,
-    paymentsTotal: Number,
-    interestRate: Number,
-    shelter: Number,
-    insurance: Number,
+    ownerid: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    efficiency: {
+        type: Number,
+        required: true
+    },
+    efficiencyUnit: {
+        type: String,
+        enum: ['mpg', 'hp'],
+        default: 'mpg'
+    },
+    haulingCapacity: {
+        type: Number,
+        required: true
+    },
+    haulingCapacityUnits: {
+        type: String,
+        enum: ['lbs', "kgs"],
+        default: "lbs"
+    },
+    workingWidth: {
+        type: Number,
+        required: true
+    },
+    operatingCost: {
+        type: Number,
+        required: true
+    },
+    operatingCostUnits: {
+        type: String,
+        enum: ['acre', "hour"],
+        default: "hour"
+    },
+    purchaseDate: {
+        type: Date,
+        required: true
+    },
+    purchasePrice: {
+        type: Number,
+        default: 0
+    },
+    salvagePrice: {
+        type: Number,
+        default: 0
+    },
+    depriciation: {
+        type: Number,
+        default: 0
+    },
+    paymentsPerYear: {
+        type: Number,
+        default: 0
+    },
+    paymentsTotal: {
+        type: Number,
+        default: 0
+    },
+    interestRate: {
+        type: Number,
+        default: 0
+    },
+    shelter: {
+        type: Number,
+        default: 0
+    },
+    insurance: {
+        type: Number,
+        default: 0
+    },
     notes: [
         {
             created_at: {
                 type: Date,
                 default: Date.now()
             },
-            text: String
+            text: String,
+            default: []
         }
     ]
 
--- routes/api/machines.js
+++ routes/api/machines.js
@@ -7,7 +7,32 @@ const Machines = mongoose.model('Machines');
 //POST create new machine, auth required
 router.post('/', auth.required, (req, res, next) => {
     const { payload: { id } } = req;
+    const { body: {machine}} = req;
     console.log(req.body);
+    //validate input
+    if(!machine){
+        return res.status(422).json({
+            errors: [
+                "machine is required with fields"
+            ]
+        })
+    }
+    //make sure machine name does not already exist for user
+    Machines.find({ownerid: id, name: machine.name})
+    .then((result) => {
+        if(result.length > 0){
+            return res.status(443).json({errors: ["A machine with that name already exists"]});
+        } else{
+            machine.ownerid = id;
+            const newMachine = new Machines(machine);
+            return newMachine.save().then(() => {
+                res.json({ machine: newMachine._id });
+            }).catch((err) => {
+                res.status(443).json(err);
+            });
+        }
+    })
+
     /*Machines.find({ownerid: id})
     .then((result) => {
         return res.status(201).json(result);
